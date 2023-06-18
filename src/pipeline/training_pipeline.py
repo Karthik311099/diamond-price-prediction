e8e9e2a6f66dc9f6b3ad894c29de5ff2fe87ea36
Karthik311099 <karthikm3110@hotmail.com>
2023-06-18 14:55:17 +0530

    flask application created

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -10,14 +10,13 @@ from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 
 
-if __name__=='__main__':
+'''if __name__=='__main__':
     obj=DataIngestion()
     train_data_path,test_data_path=obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
     train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
     model_trainer=ModelTrainer()
-    model_trainer.initate_model_training(train_arr,test_arr)
-
+    model_trainer.initate_model_training(train_arr,test_arr)'''
 
 
 
